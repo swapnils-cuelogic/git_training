8115be0a5b0dde0b28c4b8540a4a13f5d69397c1
AMEY-PC\AMEY <swap.m.sonar@gmail.com>
2017-11-14 23:22:30 +0530

    added sample program to repositary

---
+++ src/com/cuelogic/java/SampleProgram.java
@@ -0,0 +1,10 @@
+package com.cuelogic.java;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Sample Program");
+	}
+
+}
